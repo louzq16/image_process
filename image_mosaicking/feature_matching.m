ab476ddfbd683eb8389624102776802b6a3ec7f7
lzq <louzq16@mails.tsinghua.edu.cn>
2018-10-26 14:41:55 +0800

    week6

---
+++ image_mosaicking/feature_matching.m
@@ -0,0 +1,17 @@
+function output = feature_matching(address,type)
+%myFun - Description
+%多种特征点匹配算法 type=1:SURF 
+% Syntax: output = feature_matching(address,type)
+%
+% Long description
+img=Imread(address)
+if type==1
+
+else
+
+end
+end
+
+function
+    %高斯滤波 
+end
